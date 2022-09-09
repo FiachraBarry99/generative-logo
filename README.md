8677870c6fe646fc84253d13362f2cf3da09177f
FiachraBarry99 <77028625+FiachraBarry99@users.noreply.github.com>
2022-09-09 20:18:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ptu5yq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ptu5yq)
---
+++ index.js
@@ -0,0 +1,208 @@
+const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const Color = require('canvas-sketch-util/color');
+
+const settings = {
+  dimensions: [1080, 1080],
+  animate: false,
+};
+
+const sketch = ({ context, width, height }) => {
+  const num = 100;
+  const degrees = -25;
+  const size = 1.5;
+
+  let x, y, w, h, fill, stroke;
+  const rects = [];
+
+  const colours = [
+    '#0094C4',
+    '#0094C4',
+    '#0094C4',
+    '#E9D800',
+    '#E9D800',
+    '#E9D800',
+    '#7AC5DE',
+    '#7AC5DE',
+  ];
+
+  for (let i = 0; i < num; i++) {
+    context.save();
+
+    x = random.range(0, width);
+    y = random.range(0, height);
+    w = random.range(100 * size, 400 * size);
+    h = random.range(30 * size, 80 * size);
+
+    fill = random.pick(colours);
+    stroke = random.pick(colours);
+
+    blend = random.value(0, 1) > 0.5 ? 'overlay' : 'source-over';
+
+    rects.push({ x, y, w, h, stroke, fill, blend });
+  }
+
+  return ({ context, width, height }) => {
+    context.save();
+
+    context.lineWidth = 8;
+
+    context.translate(width * 0.5 - 317, width * 0.5 - 92.25);
+    shee = drawShee(context, 'rgba(0,0,0,1)');
+
+    context.clip(shee);
+
+    context.fillStyle = '#7AC5DE';
+    context.fillRect(0, 0, width, height);
+
+    rects.forEach((rect) => {
+      const { x, y, w, h, stroke, fill, blend } = rect;
+
+      context.save();
+      context.translate(-(width * 0.5 - 317), -(width * 0.5 - 92.25));
+      context.translate(x, y);
+
+      drawSkewedRect({ context, w, h, degrees, stroke, fill, blend });
+
+      context.restore();
+    });
+
+    context.restore();
+
+    context.save();
+
+    context.lineWidth = 8;
+
+    context.globalCompositeOperation = 'color-burn';
+
+    context.translate(width * 0.5 - 317, width * 0.5 - 92.25);
+    shee = drawShee(context, 'rgba(0,0,0,0.6)');
+  };
+};
+
+const drawSkewedRect = ({
+  context,
+  w = 500,
+  h = 110,
+  degrees = 25,
+  stroke = 'black',
+  fill = 'blue',
+  blend = 'source-over',
+}) => {
+  const angle = math.degToRad(degrees);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
+
+  context.save();
+  context.translate(rx * -0.5, (ry + h) * -0.5);
+  context.fillStyle = fill;
+  context.strokeStyle = stroke;
+  context.lineWidth = 10;
+
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry);
+  context.lineTo(rx, ry + h);
+  context.lineTo(0, h);
+  context.closePath();
+
+  let shadowColor = Color.offsetHSL(fill, 0, 10, -40);
+  shadowColor.rgba[3] = random.range(0.2, 0.6);
+
+  context.shadowColor = Color.style(shadowColor.rgba);
+  context.shadowOffsetX = -10;
+  context.shadowOffsetY = 20;
+
+  context.globalCompositeOperation = blend;
+  context.fill();
+
+  context.shadowColor = 'null';
+  context.stroke();
+
+  context.globalCompositeOperation = 'source-over';
+  context.strokeStyle = 'black';
+  context.lineWidth = 2;
+  context.stroke();
+
+  context.restore();
+};
+
+const drawShee = function (ctx, stroke) {
+  ctx.strokeStyle = stroke;
+  ctx.lineJoin = 'round';
+  ctx.miterLimit = 4;
+
+  let shee = new Path2D();
+  shee.moveTo(1, 112.5);
+  shee.lineTo(1, 1);
+  shee.lineTo(142, 1);
+  shee.lineTo(142, 36);
+  shee.lineTo(36, 36);
+  shee.lineTo(36, 77.5);
+  shee.lineTo(142, 77.5);
+  shee.lineTo(142, 184.5);
+  shee.lineTo(1, 184.5);
+  shee.lineTo(1, 149.5);
+  shee.lineTo(107, 149.5);
+  shee.lineTo(107, 112.5);
+  shee.lineTo(1, 112.5);
+  shee.closePath();
+
+  let hhh = new Path2D();
+  hhh.moveTo(165, 184.5);
+  hhh.lineTo(165, 1.00001);
+  hhh.lineTo(200, 1.00001);
+  hhh.lineTo(200, 79.5);
+  hhh.lineTo(271, 79.5);
+  hhh.lineTo(271, 1.00001);
+  hhh.lineTo(306, 1.00001);
+  hhh.lineTo(306, 184.5);
+  hhh.lineTo(271, 184.5);
+  hhh.lineTo(271, 114.5);
+  hhh.lineTo(200, 114.5);
+  hhh.lineTo(200, 184.5);
+  hhh.lineTo(165, 184.5);
+  hhh.closePath();
+  shee.addPath(hhh);
+
+  let e1 = new Path2D();
+  e1.moveTo(329, 184.5);
+  e1.lineTo(329, 1);
+  e1.lineTo(470, 1);
+  e1.lineTo(470, 36);
+  e1.lineTo(364, 36);
+  e1.lineTo(364, 79.5);
+  e1.lineTo(440.5, 79.5);
+  e1.lineTo(440.5, 114.5);
+  e1.lineTo(364, 114.5);
+  e1.lineTo(364, 149.5);
+  e1.lineTo(470, 149.5);
+  e1.lineTo(470, 184.5);
+  e1.lineTo(329, 184.5);
+  e1.closePath();
+  shee.addPath(e1);
+
+  let e2 = new Path2D();
+  e2.moveTo(493, 184.5);
+  e2.lineTo(493, 1);
+  e2.lineTo(634, 1);
+  e2.lineTo(634, 36);
+  e2.lineTo(528, 36);
+  e2.lineTo(528, 79.5);
+  e2.lineTo(604.5, 79.5);
+  e2.lineTo(604.5, 114.5);
+  e2.lineTo(528, 114.5);
+  e2.lineTo(528, 149.5);
+  e2.lineTo(634, 149.5);
+  e2.lineTo(634, 184.5);
+  e2.lineTo(493, 184.5);
+  e2.closePath();
+  shee.addPath(e2);
+
+  ctx.stroke(shee);
+
+  return shee;
+};
+
+canvasSketch(sketch, settings);
